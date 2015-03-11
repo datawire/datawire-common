2dfef0cfdfc794f219e20cb9472338b0a5f1b296
Abhay Saxena <ark3@email.com>
2015-03-11 15:51:42 -0400

    Null logging handler to avoid "No handler" message.

--- datawire/__init__.py
+++ datawire/__init__.py
@@ -1,5 +1,9 @@
 # Copyright (C) k736, inc. All Rights Reserved.
-# Unauthorized copying or redistribution of this file is strictly prohibited. 
+# Unauthorized copying or redistribution of this file is strictly prohibited.
+
+# So logging doesn't emit "No handlers could be found for logger" message.
+import logging as _logging
+_logging.getLogger("datawire").addHandler(_logging.NullHandler())
 
 from .address import Address
 from .container import ancestors, Container
