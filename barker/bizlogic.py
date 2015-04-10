8e677e7f6b6a6a30f423cb8836c19b896c6e5ddd
Rafael Schloming <rhs@alum.mit.edu>
2015-04-09 23:09:36 -0400

    added an agent to the bizl

--- barker/bizlogic.py
+++ barker/bizlogic.py
@@ -7,7 +7,7 @@
 """
 
 from proton.reactor import Reactor
-from datawire import Linker, Sender, Tether, Processor
+from datawire import Agent, Container, Linker, Sender, Tether, Processor
 
 import common
 
@@ -16,14 +16,18 @@ class BizLogic(object):
     def __init__(self):
         self.host = "localhost"
         self.port = 5680
-        self.tether = Tether(None, "//%s/bizlogic" % self.host, "//%s:%s" % (self.host, self.port))
+        self.tether = Tether(None, "//%s/bizlogic" % self.host, None,
+                             host=self.host, port=self.port, agent_type="bizlogic")
 
         self.users = {}
         self.user_reread_period = 30  # seconds
         self.linker = Linker()
+        self.container = Container(Processor(self))
+        self.container[self.tether.agent] = Agent(self.tether)
+        self.handlers = [self.container]
 
     def on_reactor_init(self, event):
-        event.reactor.acceptor(self.host, self.port, Processor(self))
+        event.reactor.acceptor(self.host, self.port)
         self.tether.start(event.reactor)
         event.reactor.schedule(0, self)
         self.linker.start(event.reactor)
