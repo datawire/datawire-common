9acfb1ec6b9d1a5936d343c22625ed247ffeff1c
Richard Li <richard@datawire.io>
2015-05-08 13:30:47 -0400

    fixes for sdist

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
 setup(name='datawire',
-      version='0.1',
+      version='0.2',
       description='Infrastructure for dataflow-driven, resilient microservices',
       author='datawire.io',
+      author_email='hello@datawire.io',
       url='http://www.datawire.io',
       py_modules=['common', 'service'],
       packages=['datawire'],
