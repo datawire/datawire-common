314de8bbd421af98fa6ae58ae844d82246f4fd08
Bozo Dragojevic <bozzo@digiverse.si>
2015-07-09 22:10:21 +0200

    Add option for starting nodejs proxies only
    Useful when running other bits from within eclipse

--- monitoring/launch.py
+++ monitoring/launch.py
@@ -5,7 +5,7 @@ from time import sleep
 
 commands = """
 python monitor.py --host %(hostname)s
-../barker/webui/proxy/proxy.js --thost %(hostname)s -p 5700 -t 6000
+%(webui_proxy)s --thost %(hostname)s -p 5700 -t 6000
 """
 
 def launch(command):
@@ -23,9 +23,12 @@ def launch(command):
 def main():
     parser = ArgumentParser()
     parser.add_argument("-n", "--host", default="127.0.0.1", help="network hostname")
+    parser.add_argument("--webui-only", default=False, action="store_true", help="start only webui helpers")
+    parser.add_argument("--webui-proxy", default="../barker/webui/proxy/proxy.js", help="location of the proxy")
     args = parser.parse_args()
 
-    params = dict(hostname=args.host)
+    params = dict(hostname=args.host,
+                  webui_proxy=args.webui_proxy)
 
     with open("monitor_host.js", "wb") as jsfile:
         jsfile.write("""monitor_host = "%(hostname)s";\n""" % params)
@@ -33,7 +36,7 @@ def main():
     pids = []
     try:
         for command in (commands % params).split("\n"):
-            if command and not command.strip().startswith("#"):
+            if command and not command.strip().startswith("#") and (command.strip().startswith(args.webui_proxy) or not args.webui_only):
                 pids.append(launch(command.strip()))
         while True:
             sleep(100000)
