3427847364094f349d96a53e5b44f66671d530f2
Bozo Dragojevic <bozzo@digiverse.si>
2015-07-10 10:00:13 +0200

    Add a jython test for coverage of Java API implementation

--- datawire/impl.py
+++ datawire/impl.py
@@ -1,10 +1,12 @@
 # Copyright (C) k736, inc. All Rights Reserved.
 # Unauthorized copying or redistribution of this file is strictly prohibited.
 
+dualImpls = set()
 try:
   import jdatawire
   def dual_impl(clazz):
     if hasattr(jdatawire, clazz.__name__):
+      dualImpls.add(clazz.__name__)
       return getattr(jdatawire, clazz.__name__)
     else:
       return clazz
---
+++ datawire/tests/impl.py
@@ -0,0 +1,35 @@
+# Copyright (C) k736, inc. All Rights Reserved.
+# Unauthorized copying or redistribution of this file is strictly prohibited.
+import datawire
+from unittest import TestCase
+from warnings import warn
+from datawire.impl import dualImpls
+
+class ImplTest(TestCase):
+
+  def assertIsJavaImpl(self, clazz):
+    self.assertRegexpMatches(clazz.__module__, "^(io.datawire|jdatawire)", "Expected %s.%s to be a Java impl" % (clazz.__module__, clazz.__name__))
+
+  def assertIsPythonImpl(self, clazz):
+    name = "%s.%s" % (clazz.__module__, clazz.__name__)
+    warn("Missing Java implementation of class %s" % name, stacklevel=2)
+    self.assertRegexpMatches(clazz.__module__, "^(datawire)", "Expected %s to be a datawire Python impl" % name)
+
+  def checkRunningOnJython(self):
+    try:
+      import jdatawire
+    except:
+      import sys
+      self.assertNotEquals(sys.platform, "jython")
+      self.skipTest("Not a jython run")
+
+  def testDualImplCoverage(self):
+    self.checkRunningOnJython()
+    datawireImpls = set(n for n in dir(datawire) if n[:1].isupper())
+    pythonImpls = datawireImpls - dualImpls
+    def clazzof(impls):
+      return (getattr(datawire, name) for name in impls)
+    for clazz in clazzof(dualImpls):
+      self.assertIsJavaImpl(clazz)
+    for clazz in clazzof(pythonImpls):
+      self.assertIsPythonImpl(clazz)
