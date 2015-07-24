24236cfd1c927c36f280bd5c98103d07b2adda21
Bozo Dragojevic <bozzo@digiverse.si>
2015-07-24 11:35:53 +0200

    Javadoc Address class

--- datawire-j/src/main/java/io/datawire/Address.java
+++ datawire-j/src/main/java/io/datawire/Address.java
@@ -1,19 +1,50 @@
+/**
+ * Copyright (C) k736, inc. All Rights Reserved.
+ * Unauthorized copying or redistribution of this file is strictly prohibited.
+ */
 package io.datawire;
 
+/**
+ * AMQP address
+ * 
+ * @author bozzo
+ *
+ */
 public final class Address {
+    /**
+     * Default port for datawire services
+     */
+    public static final String DEFAULT_PORT = "5672";
     private String _text;
     private String _network;
     private String _host;
     private String _port;
-    
+
+    /**
+     * Parse the supplied text into an AMQP address object
+     * 
+     * @param text
+     *            String representation of the address to parse. Can be
+     *            {@literal null}
+     * @return an {@link Address} instance or {@literal null} if
+     *         {@literal null was supplied}
+     */
     public static Address parse(String text) {
         if (text == null) {
-            return null;
+            return null; // XXX: Would it not be better to return a static
+                         // instance of Address(null)
         } else {
             return new Address(text);
         }
     }
-    
+
+    /**
+     * An address instance containing parsed text
+     * 
+     * @param text
+     *            Address to parse. When {@literal null} is supplied all
+     *            instance getters will return null.
+     */
     public Address(String text) {
         this._text = text;
         _network = networkOfAddress(text);
@@ -22,6 +53,12 @@ public final class Address {
         _port = hostport[1];
     }
 
+    /**
+     * For addresses of the form {@literal "//<network>][/...]"} returns the network address part
+     * @param address
+     *  the address to parse, can be {@literal null}
+     * @return the extracted network address part or {@literal null} if the parameter is null or the address does not start with "//"
+     */
     public static String networkOfAddress(String address) {
         if (address == null)
             return null;
@@ -31,32 +68,53 @@ public final class Address {
         return null;
     }
 
+    /**
+     * split the network part of the address into host and port parts. If the address does not specify port the {@link Address#DEFAULT_PORT} is returned as port.
+     * @param network Can be null
+     * @return Array of two Strings, representing the tuple [host, port]. A {@literal null} network will result in an array of two {@literal null} strings.
+     */
     public static String[] hostportOfNetwork(String network) {
         if (network == null)
-            return new String[2];
+            return new String[] { null, null };
         if (network.contains(":")) {
-            return network.split(":",2);
+            return network.split(":", 2);
         } else {
-            return new String[] {network, "5672"};
+            return new String[] { network, DEFAULT_PORT };
         }
     }
 
+    /**
+     * The whole address as supplied to the {@link Address#Address(String)} constructor
+     * @return the address as supplied
+     */
     public String getText() {
         return _text;
     }
 
+    /**
+     * The network part of an address of the form {@literal "//<network>[...]"}
+     * @return The network part or {@literal null} if it's not a network address
+     */
     public String getNetwork() {
         return _network;
     }
 
+    /**
+     * The host of the network part of an address of the form {@literal "//<network>[...]"}
+     * @return The host or {@literal null} if it's not a network address
+     */
     public String getHost() {
         return _host;
     }
 
+    /**
+     * The port of the network part of an address of the form {@literal "//<network>[...]"}
+     * @return The port or {@literal null} if it's not a network address
+     */
     public String getPort() {
         return _port;
     }
-    
+
     @Override
     public String toString() {
         return _text;
