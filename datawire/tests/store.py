8643030cdfb1ed6d7fb3742e5cf781094f63a67b
Bozo Dragojevic <bozzo@digiverse.si>
2015-09-23 20:33:32 +0200

    a few Store tests

---
+++ datawire/tests/store.py
@@ -0,0 +1,43 @@
+# Copyright (C) k736, inc. All Rights Reserved.
+# Unauthorized copying or redistribution of this file is strictly prohibited.
+
+from unittest import TestCase
+from proton import Message
+from proton.reactor import Reactor
+from datawire import Store, Entry
+
+from .common import *
+
+class StoreTest(TestCase):
+  
+  def setUp(self):
+    self.store = Store()
+
+  def testEmpty(self):
+    r = self.store.reader()
+    self.assertFalse(r.more(), "Empty store should be empty")
+
+  def testPlain(self):
+    self.store.put("hello")
+    r = self.store.reader()
+    self.assertTrue(r.more(), "Store should not be empty")
+    e = r.next()
+    self.assertFalse(r.more(), "Store should be empty")
+    print e
+
+  def testGc(self):
+    map(self.store.put, "A B C D E F".split())
+    r = self.store.reader()
+    r.next()
+    r.next()
+    r.next()
+    self.store.gc()
+
+class DerivedStore(Store):
+  def __init__(self, name=None):
+    Store.__init__(self, name)
+
+class DerivedStoreTest(StoreTest):
+
+  def setUp(self):
+    self.store = DerivedStore()
