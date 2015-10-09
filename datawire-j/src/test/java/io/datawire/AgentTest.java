20ad57c5c78f15ffe100eb45858c8cf87eb5e3ad
Bozo Dragojevic <bozzo@digiverse.si>
2015-10-09 10:07:43 +0200

    Add licence header

--- datawire-j/src/test/java/io/datawire/AgentTest.java
+++ datawire-j/src/test/java/io/datawire/AgentTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2015 datawire. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.datawire;
 
 import static org.junit.Assert.*;
@@ -54,7 +69,6 @@ public class AgentTest {
         agent.getSampler().setFrequency(frequency);
         final Receiver receiver = Receiver.Builder().withSource(server.address()).create();;
         class Counter extends Timeout {
-            public boolean openEvent = false;
             private int received = 0;
             public void onTimerTask(org.apache.qpid.proton.engine.Event e) {
                 receiver.stop(e.getReactor());
